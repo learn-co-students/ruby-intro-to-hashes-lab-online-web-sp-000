94d69c674b05b68e5ad38c5e79630de6cccf3829
Danny Brandt <dvbrandt90@gmail.com>
2020-12-17 03:52:50 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,8 +22,7 @@ def monopoly_with_second_tier
 	      :pieces => 4,
 	      :names => {}, 
 	      :rent_in_dollars => {}
-	    },
-	    :names => {}
+	    }
 	}
 end
 
@@ -32,7 +31,6 @@ def monopoly_with_third_tier
    	monopoly = {
 	    :railroads => {
 	      :pieces => 4,
-	      :names => {}, 
 	      :rent_in_dollars => {
 	        :one_piece_owned => 25,
 	        :two_pieces_owned => 50,
@@ -54,7 +52,6 @@ def monopoly_with_fourth_tier
       monopoly = {
 	    :railroads => {
 	      :pieces => 4,
-	      :names => {}, 
 	      :rent_in_dollars => {
 	        :one_piece_owned => 25,
 	        :two_pieces_owned => 50,
